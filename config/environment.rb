cd3638ed57cf073e75f9a867eb59593958f968b8
Christopher Cotton <me@christophercotton.com>
2011-05-26 16:17:29 -0500

    trying to turn on emails

--- config/environment.rb
+++ config/environment.rb
@@ -48,7 +48,7 @@ Rails::Initializer.run do |config|
   # Deliveries are disabled by default. Do NOT modify this section.
   # Define your email configuration in configuration.yml instead.
   # It will automatically turn deliveries on
-  config.action_mailer.perform_deliveries = false
+  # config.action_mailer.perform_deliveries = false
 
   config.gem 'rubytree', :lib => 'tree'
   config.gem 'coderay', :version => '~>0.9.7'
